1b5972f051aafd8d5ecc0a18531d8a342b7d801d
Michael <mechiland@gmail.com>
2008-11-20 00:21:07 +0800

    initial structure

---
+++ caihong/src/test/java/caihong/AppTest.java
@@ -0,0 +1,21 @@
+package caihong;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+public class AppTest 
+{
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test2() {
+        assertFalse(true);
+    }
+}
