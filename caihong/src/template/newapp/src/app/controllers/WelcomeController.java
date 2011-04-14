f83a891d133c52bccc4fe9065d9545a4b52d1272
Michael Chen <mechiland@gmail.com>
2011-04-15 00:31:43 +0800

    add missing files

---
+++ caihong/src/template/newapp/src/app/controllers/WelcomeController.java
@@ -0,0 +1,9 @@
+package app.controllers;
+
+import org.javaonhorse.web.Controller;
+
+public class WelcomeController extends Controller {
+
+    public void index() { }
+
+}
---
+++ caihong/src/template/newapp/test/unit/app/controllers/HelloTest.java
@@ -0,0 +1,12 @@
+package app.controllers;
+
+import org.junit.Test;
+
+public class HelloTest {
+  
+  @Test
+  public void should_be_true() {
+    //
+  }
+  
+}
